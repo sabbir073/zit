57e4d6c5b5b104fc8a7112dcb79f2b23ae250eb0
sabbir073 <md.sabbir073@gmail.com>
2023-10-10 02:57:19 +0600

    vds update

--- pages/vds.tsx
+++ pages/vds.tsx
@@ -5,6 +5,7 @@ import Tippy from '@tippyjs/react';
 import 'tippy.js/dist/tippy.css';
 import { setPageTitle } from '../store/themeConfigSlice';
 import { useDispatch } from 'react-redux';
+import Select from 'react-select';
 
 const rowData = [
     {
@@ -12,6 +13,55 @@ const rowData = [
         supplier_name: 'Rahim Varieties Store',
         treasure_challan_no: 'TCM-23-00001',
         certificate_no: '00001',
+        transaction_date: '10-04-23',
+    },
+    {
+        id: 2,
+        supplier_name: 'Rahim Varieties Store',
+        treasure_challan_no: 'TCM-23-00002',
+        certificate_no: '00002',
+        transaction_date: '10-04-23',
+    },
+    {
+        id: 3,
+        supplier_name: 'Rahim Varieties Store',
+        treasure_challan_no: 'TCM-23-00003',
+        certificate_no: '00003',
+        transaction_date: '10-04-23',
+    },
+    {
+        id: 4,
+        supplier_name: 'Rahim Varieties Store',
+        treasure_challan_no: 'TCM-23-00004',
+        certificate_no: '00004',
+        transaction_date: '10-04-23',
+    },
+    {
+        id: 5,
+        supplier_name: 'Rahim Varieties Store',
+        treasure_challan_no: 'TCM-23-00005',
+        certificate_no: '00005',
+        transaction_date: '10-04-23',
+    },
+    {
+        id: 6,
+        supplier_name: 'Rahim Varieties Store',
+        treasure_challan_no: 'TCM-23-00006',
+        certificate_no: '00006',
+        transaction_date: '10-04-23',
+    },
+    {
+        id: 7,
+        supplier_name: 'Rahim Varieties Store',
+        treasure_challan_no: 'TCM-23-00007',
+        certificate_no: '00007',
+        transaction_date: '10-04-23',
+    },
+    {
+        id: 8,
+        supplier_name: 'Rahim Varieties Store',
+        treasure_challan_no: 'TCM-23-00008',
+        certificate_no: '00008',
         transaction_date: '10-04-23',
     },
 ];
@@ -68,16 +118,33 @@ const MultipleTables = () => {
         setPage(1);
     }, [sortStatus]);
 
+    const options3 = [
+        { value: 'orange', label: 'Orange' },
+        { value: 'white', label: 'White' },
+        { value: 'purple', label: 'Purple' },
+    ];
+
     return (
         <div>
 
             <div className="panel mt-5">
                 <div className="mb-5 flex flex-col gap-5 md:flex-row md:items-center">
                     <h5 className="text-lg font-semibold dark:text-white-light">6D_VDS For Purchaser</h5>
-                    <div className="ltr:ml-auto rtl:mr-auto">
+                    
+                </div>
+                <div className="mb-5 flex flex-col gap-5 md:flex-row md:items-center">
+                <button type="button" className="btn btn-primary">
+                <svg width="20" height="20" viewBox="0 0 24 24" stroke="currentColor" stroke-width="1.5" fill="none" stroke-linecap="round" stroke-linejoin="round" class="w-6 h-6"><line x1="12" y1="5" x2="12" y2="19"></line><line x1="5" y1="12" x2="19" y2="12"></line></svg>
+                                Add New
+                            </button>
+                    <div className="ltr:ml-auto">
+                    <Select placeholder="Select an option" options={options3} />
+                    </div>
+                    <div className="rtl:mr-auto">
                         <input type="text" className="form-input w-auto" placeholder="Search..." value={search} onChange={(e) => setSearch(e.target.value)} />
                     </div>
                 </div>
+                
                 <div className="datatables">
                     {isMounted && (
                         <DataTable
@@ -143,41 +210,10 @@ const MultipleTables = () => {
                                     titleClassName: '!text-center',
                                     render: () => (
                                         <div className="mx-auto flex w-max items-center gap-2">
-                                            <Tippy content="Edit">
-                                                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className="h-5 w-5">
-                                                    <path
-                                                        d="M15.2869 3.15178L14.3601 4.07866L5.83882 12.5999L5.83881 12.5999C5.26166 13.1771 4.97308 13.4656 4.7249 13.7838C4.43213 14.1592 4.18114 14.5653 3.97634 14.995C3.80273 15.3593 3.67368 15.7465 3.41556 16.5208L2.32181 19.8021L2.05445 20.6042C1.92743 20.9852 2.0266 21.4053 2.31063 21.6894C2.59466 21.9734 3.01478 22.0726 3.39584 21.9456L4.19792 21.6782L7.47918 20.5844L7.47919 20.5844C8.25353 20.3263 8.6407 20.1973 9.00498 20.0237C9.43469 19.8189 9.84082 19.5679 10.2162 19.2751C10.5344 19.0269 10.8229 18.7383 11.4001 18.1612L11.4001 18.1612L19.9213 9.63993L20.8482 8.71306C22.3839 7.17735 22.3839 4.68748 20.8482 3.15178C19.3125 1.61607 16.8226 1.61607 15.2869 3.15178Z"
-                                                        stroke="currentColor"
-                                                        strokeWidth="1.5"
-                                                    />
-                                                    <path
-                                                        opacity="0.5"
-                                                        d="M14.36 4.07812C14.36 4.07812 14.4759 6.04774 16.2138 7.78564C17.9517 9.52354 19.9213 9.6394 19.9213 9.6394M4.19789 21.6777L2.32178 19.8015"
-                                                        stroke="currentColor"
-                                                        strokeWidth="1.5"
-                                                    />
-                                                </svg>
+                                            <Tippy content="View">
+                                            <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="w-6 h-6"><path opacity="0.5" d="M3.27489 15.2957C2.42496 14.1915 2 13.6394 2 12C2 10.3606 2.42496 9.80853 3.27489 8.70433C4.97196 6.49956 7.81811 4 12 4C16.1819 4 19.028 6.49956 20.7251 8.70433C21.575 9.80853 22 10.3606 22 12C22 13.6394 21.575 14.1915 20.7251 15.2957C19.028 17.5004 16.1819 20 12 20C7.81811 20 4.97196 17.5004 3.27489 15.2957Z" stroke="currentColor" stroke-width="1.5"></path><path d="M15 12C15 13.6569 13.6569 15 12 15C10.3431 15 9 13.6569 9 12C9 10.3431 10.3431 9 12 9C13.6569 9 15 10.3431 15 12Z" stroke="currentColor" stroke-width="1.5"></path></svg>
                                             </Tippy>
-                                            <Tippy content="Delete">
-                                                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className="h-5 w-5">
-                                                    <path
-                                                        opacity="0.5"
-                                                        d="M9.17065 4C9.58249 2.83481 10.6937 2 11.9999 2C13.3062 2 14.4174 2.83481 14.8292 4"
-                                                        stroke="currentColor"
-                                                        strokeWidth="1.5"
-                                                        strokeLinecap="round"
-                                                    />
-                                                    <path d="M20.5001 6H3.5" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" />
-                                                    <path
-                                                        d="M18.8334 8.5L18.3735 15.3991C18.1965 18.054 18.108 19.3815 17.243 20.1907C16.378 21 15.0476 21 12.3868 21H11.6134C8.9526 21 7.6222 21 6.75719 20.1907C5.89218 19.3815 5.80368 18.054 5.62669 15.3991L5.16675 8.5"
-                                                        stroke="currentColor"
-                                                        strokeWidth="1.5"
-                                                        strokeLinecap="round"
-                                                    />
-                                                    <path opacity="0.5" d="M9.5 11L10 16" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" />
-                                                    <path opacity="0.5" d="M14.5 11L14 16" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" />
-                                                </svg>
-                                            </Tippy>
+                                            
                                         </div>
                                     ),
                                 },
